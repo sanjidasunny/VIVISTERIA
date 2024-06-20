c0c00df0f8fbd861e18b3c232126c87d64767624
Sunny <sanjidasunny25@gmail.com>
2024-06-20 08:04:06 +0600

    try8

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const { query } = require('express-validator');
 const mongoDB = require('./database');
-const createUser = require('./Routes/CreateUser'); 
+const createUser = require('./Routes/CreateUser'); // Import the createUser route
 
 const app = express();
-const port = process.env.PORT||5000;
+const port = 5000;
 
 mongoDB();
 
 const corsOptions = {
   origin: 'https://vivisteria.vercel.app',
-  credential:true,
-  methods:["POST","GET","PUT","DELETE"],
+  optionsSuccessStatus: 200
 };
-app.options("",cors(corsOptions));
-app.use(cors({corsOptions}));
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/api', createUser); // 
+app.use('/api', createUser); // Use the createUser route
 
 app.get('/', query('person').notEmpty(), (req, res) => {
   res.send('Hello World!');
