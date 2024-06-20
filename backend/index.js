1447b41dd7ff6b37be99f410d1931db3a8eaf175
Sunny <sanjidasunny25@gmail.com>
2024-06-20 07:56:38 +0600

    try7

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { query } = require('express-validator');
 const mongoDB = require('./database');
-const createUser = require('./Routes/CreateUser'); // Import the createUser route
+const createUser = require('./Routes/CreateUser'); 
 
 const app = express();
 const port = process.env.PORT||5000;
@@ -18,7 +18,7 @@ app.options("",cors(corsOptions));
 app.use(cors({corsOptions}));
 app.use(express.json());
 
-app.use('/api', createUser); // Use the createUser route
+app.use('/api', createUser); // 
 
 app.get('/', query('person').notEmpty(), (req, res) => {
   res.send('Hello World!');
