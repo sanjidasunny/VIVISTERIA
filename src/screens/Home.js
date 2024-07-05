5b3c64466b9f3b5c2a8f175cce5534bf7df2d085
Sunny <sanjidasunny25@gmail.com>
2024-07-05 07:31:59 -0700

    18

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,23 +13,24 @@ function Home() {
 
   const loadData = async () => {
     try {
-      const response = await axios.get('https://vivisteria.vercel.app/api/foodData', {
+      const response = await axios.get('https://vivisteria.vercel.app/api/foodData', {}, {
         headers: {
           'Content-Type': 'application/json'
-        }
+        },
       });
-      if (response.status !== 200) {
-        throw new Error("Failed to fetch data: " + response.status);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data"+response.status);
+        
+
       }
-  
-      setFoodItem(response.data[0]); 
-      setFoodCat(response.data[1]);
+      response = await response.json();
+      setFoodItem(response[0]);
+      setFoodCat(response[1]);
     } catch (error) {
       console.error("Error fetching data:", error);
       // Handle error state here, e.g., show a message to the user
     }
   };
-  
 
   useEffect(() => {
     loadData();
