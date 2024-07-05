8f265c23621af8290a0a7f760e5eedf6fd973420
Sunny <sanjidasunny25@gmail.com>
2024-07-05 06:23:28 -0700

    6

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ mongoDB(); // Connect to MongoDB
 
 // Middleware
 app.use(cors({
-  origin: 'https://vivisteria-2lrx.vercel.app/api/foodData',
+  origin: 'https://vivisteria.vercel.app',
   methods: ['POST', 'GET', 'PUT'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
