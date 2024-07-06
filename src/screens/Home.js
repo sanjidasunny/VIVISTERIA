5412b28a9153e3b3ecbf0cd5ef7d5063a14a7602
Sunny <sanjidasunny25@gmail.com>
2024-07-06 00:45:15 -0700

    28

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,21 +13,30 @@ function Home() {
 
   const loadData = async () => {
     try {
-      const response = await axios.get('https://vivisteria.vercel.app/api/foodData');
+      const response = await fetch('https://vivisteria.vercel.app/api/foodData', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
 
-      if (response.status !== 200) {
+      if (!response.ok) {
         throw new Error('Failed to fetch data: ' + response.status);
       }
 
-      const responseData = response.data;
-      setFoodItem(responseData[0] || []);
-      setFoodCat(responseData[1] || []);
+      const responseData = await response.json();
+      setFoodItem(responseData[0]);
+      setFoodCat(responseData[1]);
 
     } catch (error) {
       console.error('Error fetching data:', error);
-      // Handle error state or message appropriately
+      console.log("Hello\n");
+      
     }
   };
+
+
+
 
   useEffect(() => {
     loadData();
@@ -35,52 +44,62 @@ function Home() {
 
   return (
     <div className="mainBody">
-      <Navbar />
-      <Search setSearch={setSearch} />
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Search setSearch={setSearch} />
+      </div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 col-md-2">
             <Sidebar />
           </div>
           <div className="col-12 col-md-10">
-            {foodCat.length > 0 ? (
-              foodCat.map((data) => (
-                <div className="row mb-3" key={data._id}>
-                  <div className="fs-3 m-3 text-success">
-                    {data.CategoryName}
+            {foodCat.length > 0
+              ? foodCat.map((data) => {
+                return (
+                  <div className="row mb-3" key={data._id}>
+                    <div className="fs-3 m-3 text-success">
+                      {data.CategoryName}
+                    </div>
+                    <hr className="text-success" />
+                    {foodItem.length > 0 ? (
+                      foodItem
+                        .filter(
+                          (item) =>
+                            item.CategoryName === data.CategoryName &&
+                            typeof search === "string" &&
+                            item.name
+                              .toLowerCase()
+                              .includes(search.toLowerCase())
+                        )
+                        .map((filterItems) => {
+                          return (
+                            <div
+                              key={filterItems._id}
+                              className="col-12 col-sm-6 col-lg-3 mb-3"
+                            >
+                              <Card
+                                foodItem={filterItems}
+                                options={filterItems.options[0]}
+                              />
+                            </div>
+                          );
+                        })
+                    ) : (
+                      <div>No Such Data Found</div>
+                    )}
                   </div>
-                  <hr className="text-success" />
-                  {foodItem.length > 0 ? (
-                    foodItem
-                      .filter(
-                        (item) =>
-                          item.CategoryName === data.CategoryName &&
-                          typeof search === "string" &&
-                          item.name.toLowerCase().includes(search.toLowerCase())
-                      )
-                      .map((filterItems) => (
-                        <div
-                          key={filterItems._id}
-                          className="col-12 col-sm-6 col-lg-3 mb-3"
-                        >
-                          <Card
-                            foodItem={filterItems}
-                            options={filterItems.options[0]}
-                          />
-                        </div>
-                      ))
-                  ) : (
-                    <div>No Such Data Found</div>
-                  )}
-                </div>
-              ))
-            ) : (
-              <div>No Categories Found</div>
-            )}
+                );
+              })
+              : ""}
           </div>
         </div>
       </div>
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
