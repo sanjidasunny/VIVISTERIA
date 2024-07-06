cc63b329627fa5327ef3dc3e092e9199b91ef4f7
Sunny <sanjidasunny25@gmail.com>
2024-07-06 12:51:08 -0700

    5

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,7 +13,7 @@ function Home() {
 
   const loadData = async () => {
     try {
-      const response = await axios.get('https://vivisteria-2lrx.vercel.app/api/foodData');
+      const response = await axios.post('https://vivisteria-2lrx.vercel.app/api/foodData');
       //hello
       const responseData = response.data;
       setFoodItem(responseData[0]);
