f62a4c5f7b18ef5c0f0cc52b4cb7ca1651a64be0
Asif A Khuda <asif13.aak@gmail.com>
2024-07-09 23:03:21 +0600

    1

--- src/screens/myOrder.js
+++ src/screens/myOrder.js
@@ -20,7 +20,7 @@ export default function MyOrder() {
         throw new Error(`Failed to fetch order data: ${response.status}`);
       }
 
-      setOrderData(response.data.orderData); 
+      setOrderData(response.data.orderData);
       console.log(response.data.orderData);
     } catch (error) {
       console.error('Error fetching order data:', error);
@@ -33,40 +33,73 @@ export default function MyOrder() {
 
   return (
     <div>
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
+
       <div className="container min-vh-100">
         <div className="row">
-          {orderData.length > 0 ? (
-            orderData.map((orderArray, index) => (
-              <div key={index} className="col-12 col-md-6 col-lg-3">
-                <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                  <div className="card-body">
-                    <h5 className="card-title text-black">Order {index + 1}</h5>
-                    {orderArray.map((order, innerIndex) => (
-                      <div key={innerIndex} className="container w-100 p-0" style={{ height: "38px" }}>
-                        {innerIndex === 0 ? (
-                          <span className="m-1 text-black">{order.Order_date}</span>
-                        ) : (
-                          <>
-                            <span className="m-1 text-black">{order.quantity}</span>
-                            <span className="m-1 text-black">{order.size}</span>
-                            <div className="d-inline ms-2 h-100 w-20 fs-5 text-black">
-                              {order.price}/-
+          {orderData != {}
+            ? Array(orderData).map((data) => {
+              return data.orderData
+                ? data.orderData.order_data
+                  .slice(0)
+                  .reverse()
+                  .map((item) => {
+                    return item.map((arrayData) => {
+                      return (
+                        <div>
+                          {arrayData.Order_date ? (
+                            <div className="m-auto mt-5 text-black">
+                              {(data = arrayData.Order_date)}
+                              <hr />
                             </div>
-                          </>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div>No order data available.</div>
-          )}
+                          ) : (
+                            <div className="col-12 col-md-6 col-lg-3">
+                              <div
+                                className="card mt-3"
+                                style={{
+                                  width: "16rem",
+                                  maxHeight: "360px",
+                                }}
+                              >
+                                <div className="card-body">
+                                  <h5 className="card-title text-black">
+                                    {arrayData.name}
+                                  </h5>
+                                  <div
+                                    className="container w-100 p-0"
+                                    style={{ height: "38px" }}
+                                  >
+                                    <span className="m-1 text-black">
+                                      {arrayData.qty}
+                                    </span>
+                                    <span className="m-1 text-black">
+                                      {arrayData.size}
+                                    </span>
+                                    <span className="m-1 text-black">
+                                      {data}
+                                    </span>
+                                    <div className=" d-inline ms-2 h-100 w-20 fs-5 text-black">
+                                      {arrayData.price}/-
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      );
+                    });
+                  })
+                : "";
+            })
+            : ""}
         </div>
       </div>
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
