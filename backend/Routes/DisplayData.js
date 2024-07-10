765a17ba6b827dd915c10690bb2baa4f470e01c0
Asif A Khuda <asif13.aak@gmail.com>
2024-07-10 12:12:53 +0600

    1

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const FoodItem = require('../models/FoodItem');
-const FoodCategory = require('../models/FoodCategory');
 
-router.post('/foodData', async (req, res) => {
+router.post('/foodData', (req, res) => {
     try {
-        const foodItems = await FoodItem.find({});
-        const foodCategories = await FoodCategory.find({});
-
-        console.log('Fetched Food Items:', foodItems);  // Log fetched food items
-        console.log('Fetched Food Categories:', foodCategories);  // Log fetched food categories
-
-        res.send([foodItems, foodCategories]);
+        // console.log(global.food_items);
+        res.send([global.food_items, global.foodCategory]);
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Server Error");
+        res.send("Server Error")
     }
-});
+})
+
+
 
 module.exports = router;
