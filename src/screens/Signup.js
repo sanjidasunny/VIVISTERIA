1a24c91471cbb18088a8fa3ba96f26901d1659e0
Sunny <sanjidasunny25@gmail.com>
2024-07-17 21:42:55 -0600

    3

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
   const [credentials, setCredentials] = useState({
