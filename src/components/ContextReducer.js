92e7aaeece254e69e4abd90269963c5cd601004e
Sunny <139641456+sanjidasunny@users.noreply.github.com>
2024-07-27 03:22:31 -0600

    Update ContextReducer.js

--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -1,7 +1,10 @@
-import React, { children, createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, useReducer, useEffect } from "react";
 
 const cartStateContext = createContext();
 const cartDispatchContext = createContext();
+
+const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || '[]');
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD":
@@ -21,17 +24,11 @@ const reducer = (state, action) => {
       newArr.splice(action.index, 1);
       return newArr;
     case "DROP":
-      let empArray = [];
-      return empArray;
+      return [];
     case "UPDATE":
       let arr = [...state];
       arr.find((food, index) => {
         if (food.id === action.id) {
-          console.log(
-            food.qty,
-            parseInt(action.qty),
-            action.price + food.price
-          );
           arr[index] = {
             ...food,
             quantity: parseInt(action.quantity) + food.quantity,
@@ -41,11 +38,18 @@ const reducer = (state, action) => {
         return arr;
       });
       return arr;
+    default:
+      return state;
   }
 };
 
 export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, []);
+  const [state, dispatch] = useReducer(reducer, cartFromLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state));
+  }, [state]);
+
   return (
     <cartDispatchContext.Provider value={dispatch}>
       <cartStateContext.Provider value={state}>
