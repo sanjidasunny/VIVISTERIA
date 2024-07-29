38687e626c85b1ec9c3f06b638e3e9ce2b74f10f
Sunny <sanjidasunny25@gmail.com>
2024-07-28 22:31:08 -0600

    c5

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axiosInstance from '../components/AxiosInstance';
 import Navbar from "../components/Navbar";
-import Loader from "../components/loader"; 
 
 function Profile() {
   const [profileData, setProfileData] = useState(null);
@@ -13,8 +12,8 @@ function Profile() {
     oldPassword: "",
     newPassword: "",
   });
-  const [loading, setLoading] = useState(false); 
-  const [error, setError] = useState(""); 
+  const [loading, setLoading] = useState(false); // State to track loading state
+  const [error, setError] = useState(""); // State to track error message
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -141,8 +140,9 @@ function Profile() {
                     </label>
                   </div>
                   <button type="submit" disabled={loading}>
-                    {loading ? <Loader /> : "Submit"} 
+                    {loading ? "Loading..." : "Submit"}
                   </button>
+
                   {error && <p className="error-message">{error}</p>}
                 </form>
               ) : (
@@ -169,7 +169,7 @@ function Profile() {
             )}
           </>
         ) : (
-          <Loader /> 
+          <p>Loading...</p>
         )}
       </div>
     </div>
