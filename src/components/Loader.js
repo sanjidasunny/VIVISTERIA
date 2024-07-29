556a2c34149a26718596c67c9a84fbae96837e54
Sunny <sanjidasunny25@gmail.com>
2024-07-28 20:56:09 -0600

    loader

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DotSpinner } from '@uiball/loaders';
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <DotSpinner size={40} speed={0.9} color="black" />
+        </div>
+    );
+};
+
+export default Loader;
