37ec62729b8a07b966cc5a5478564ac21692b8b5
Sunny <sanjidasunny25@gmail.com>
2024-07-29 00:07:13 -0600

    minor

--- src/screens/Reviews.js
+++ src/screens/Reviews.js
@@ -107,7 +107,7 @@ const Reviews = () => {
     setEditMode(true);
     setNewReview(comment);
     setEditReviewId(reviewId);
-    const yOffset = 200;
+    const yOffset = 800;
     window.scrollTo({
       top: yOffset,
       behavior: "smooth"
